077c504a9a16a183337c529ff759d4263fd8e1a1
dailycodeminskole <116961849+dailycodeminskole@users.noreply.github.com>
2023-02-07 20:42:26 +0530

    Intro and First code

---
+++ 01_CourseIntro.py
@@ -0,0 +1,53 @@
+#################################################
+#  07.02.2023
+#################################################
+#  TOPICS TO BE COVERED 
+# 👉 Syllabus Walkthrough
+# 👉 Where to write the code
+# 👉 First code
+#################################################
+
+# syllabus flow 
+
+'''
+1. Python fundamentals [2 months]
+2. Modules in Python : Numpy and Pandas , Matplotlib Intro [1.5 months]
+3. Database : MY-SQL [1 month ]
+4. Excel : basic functions, Lookups, Pivot [1 week]
+5. Visualization Tool :  Tableau /MS Power BI [1.5 months]
+
+course duration : 6months
+
+'''
+
+#  Where to write the code
+
+'''
+1. writing the code 
+        a. notepad
+        b. VS code
+        c. Jupyter Notebook /Google Colab
+     
+2. Executing the code 
+    Python Inter
+'''
+
+
+# What to refer for help
+'''
+
+# Any version after Python 3.7 can be used in the class
+https://docs.python.org/3.10/library/index.html
+
+'''
+
+
+# First code
+# print is a function
+print("Hello World")
+
+print(print) # <built-in function print>
+print(print()) # nothing > none
+print(10+20) # nothing > none
+
+
