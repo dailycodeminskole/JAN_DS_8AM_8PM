ce77f2791b304eac0f44a8f2396c72a847aa72b1
dailycodeminskole <116961849+dailycodeminskole@users.noreply.github.com>
2023-02-09 20:46:27 +0530

    8PM Rahul

---
+++ 03_Strings_1.py
@@ -0,0 +1,148 @@
+#################################################
+#  09.02.2023 8.00PM
+#################################################
+#  TOPICS TO BE COVERED 
+# 👉 Comments
+# 👉 Intro to data types
+# 👉 Strings
+#################################################
+
+
+# Revision
+
+# @coolBoy_123 = "Rafique"
+_123567 = 456 
+
+name_1 = "Rakesh"
+name_2 = "Sameer"
+name_3 = "Prakash"
+
+__name__ = "Prafulla" #not to be used , to be avoided
+
+
+x = 500
+y = 600
+z = 700
+
+
+# single line 
+
+x ,y,z = 500,600,700
+
+print(x)
+print(y)
+print(z)
+
+print(id(x))
+print(id(y))
+print(id(z))
+
+
+x = 100
+y = 100
+z = 100
+
+# same ID for all the variables 
+# since they have same values
+# proper memory manage
+
+x = y = z = 100
+print(id(x))
+print(id(y))
+print(id(z))
+
+
+
+
+# comments in python
+# Line for the coder for his/her understading 
+# not be read while execution for Python. Python will not execute the line
+
+# Single line using '#'
+# Multiline comment ''' ''' 
+
+
+# Address
+# 10a /1101
+# Chandrapur
+# Maharashtra
+# 400076
+
+
+'''
+Address
+10a /1101
+Chandrapur
+Maharashtra
+400076
+
+'''
+
+
+''' 
+this is a commnet
+this is 2nd line
+this is 3r line
+this is 4th line
+
+'''
+
+
+
+# INtro to data types
+'''
+1. Numbers
+    a. Int
+    b. Float
+    c. Complex
+2. Strings
+
+3. List
+
+4. Tuples
+
+5. Sets
+
+6. Dictionaries
+
+7. Boolean
+
+8. etc etc..
+
+
+'''
+
+
+# Intro to strings
+#  more on print finction
+# anything inside  > '' ," " ,''' ''' will be a string*
+
+
+
+print("Ramu")
+print("Ramesh" ,"Suresh") 
+# we can print n numbers of strings/variables inside the print()
+
+# print('I won't be coming tomorrow') #SyntaxError
+print("I won't be coming tomorrow") 
+
+# print("sameer said,"he was not present on the site" ") #SyntaxError
+print('''sameer said,"he was not present on the site"''')
+
+# pref  triple quotes, double quotes , single quotes
+
+# HW
+# Modify quotes in below code wherever necessary for print() to work without error
+
+print("Today is a good day")
+print("Mahatma gandhi said in his speech ,"we should work hard" ")
+print('It's a brand new car as the B'day gift')
+print("C:\Users\tanmay\docs")
+
+
+
+
+
+
+
+
